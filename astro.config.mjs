d4dd84a789309eca672763d6dae5d769368c9d41
“mariajosu” <maria.josu@student.howest.be>
2025-11-28 15:04:19 +0100

    Configure site and base for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://MariaJosu7.github.io',
-  base: '/cck/',
+  base: '/cck',
 });
